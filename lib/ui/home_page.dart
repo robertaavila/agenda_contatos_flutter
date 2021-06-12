b1f5ec4767ba810136ef2778737dfb4f6ea63061
Roberta Avila <rvc.avila@gmail.com>
2021-06-12 01:56:46 -0300

    Inserindo os cards para mostrar os contatos.

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,14 +1,14 @@
+import 'dart:io';
+
 import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   ContactHelper helper = ContactHelper();
 
   List<Contact> contacts = List();
@@ -28,8 +28,7 @@ class _HomePageState extends State<HomePage> {
 
     helper.getAllContacts().then((list) => print(list));
 
-    helper.getAllContacts().then((list) =>
-        setState(() => contacts = list));
+    helper.getAllContacts().then((list) => setState(() => contacts = list));
   }
 
   @override
@@ -48,10 +47,48 @@ class _HomePageState extends State<HomePage> {
       ),
       body: ListView.builder(
           padding: EdgeInsets.all(10.0),
-          itemCount:contacts.length,
+          itemCount: contacts.length,
           itemBuilder: (context, index) {
-
+            return _contactCard(context, index);
           }),
     );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+        child: Card(
+            child: Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          Container(
+            width: 80.0,
+            height: 80.0,
+            // decoration: BoxDecoration(
+            //     shape: BoxShape.circle,
+            //     image: contacts[index].img != null
+            //         ? FileImage(File(contacts[index].img))
+            //         : AssetImage("images/user.png")),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(contacts[index].name ?? "",
+                    style:
+                        TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold)),
+                Text(contacts[index].email ?? "",
+                    style:
+                    TextStyle(fontSize: 18.0)),
+                Text(contacts[index].phone ?? "",
+                    style:
+                    TextStyle(fontSize: 18.0)),
+              ],
+            ),
+          )
+        ],
+      ),
+    )));
   }
 }
